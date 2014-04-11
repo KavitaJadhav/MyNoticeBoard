3a3e01de8d774eb1dd2fac6885f7bd34b384c1e4
Kavita Jadhav <kavitama@INkavitama.local>
2014-04-11 09:51:23 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyNoticeBoard::Application.config.secret_token = '3f957bff95f95597190948c331c666037bdc8cb3799374f8aa22ba04658c507bc742b91d965fc9860c0180c9abf41fcab4a1315cc5631760893ca03dcbbfd274'
